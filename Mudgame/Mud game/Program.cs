b01fbd5a6c98d7fe1aac0bafb07c222fb59e7b05
unknown <rgn7jjw@gmail.com>
2025-10-26 11:49:49 +0900

    Scene, IRender

--- Mudgame/Mud game/Program.cs
+++ Mudgame/Mud game/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Mud_game;
+
+class SubScene : Scene, IRenderer
+{
+    public override void Show()
+    {
+        Console.WriteLine("서브");
+        Console.ReadKey();
+    }
+}//예시로 만든 클래스
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Scene currentScene = new MainScene();//시작씬
+
+        while (currentScene.isGameContinue)
+        {
+            Console.Clear();
+            currentScene.Show();
+
+            if (currentScene.nextScene != null)
+            {
+                //만약에 현재의 씬에서 다른씬으로 넘어가는 조건이 만족이된다면 다음씬으로 이동
+                currentScene = currentScene.nextScene;
+            }
+
+        }
+
+        Console.WriteLine("게임이 종료되었습니다.");
+    }
+}
---
+++ Mudgame/Mud game/Scene.cs
@@ -0,0 +1,22 @@
+﻿namespace Mud_game
+{
+    class Scene : IRenderer //모든 씬의 부모
+    {
+        public Scene nextScene = null;
+        public bool isGameContinue = true;
+        public virtual void Show()
+        {
+
+        }
+
+        public void ChangeScene(Scene newScene) //씬 바꾸는 함수
+        {
+            nextScene = newScene;
+        }
+
+        public void ExitGame() // 게임 멈추는 함수
+        {
+            isGameContinue = false;
+        }
+    }
+}
